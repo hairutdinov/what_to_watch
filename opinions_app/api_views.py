04cf5a2019c494d61d9bedfa9e1846913f0fb6c0
Bulat <bulatxajrutdinoff@yandex.ru>
2025-11-16 22:57:03 +0300

    api

---
+++ opinions_app/api_views.py
@@ -0,0 +1,70 @@
+from random import randrange
+
+from flask import jsonify, request
+
+from . import app, db
+from .models import Opinion
+from .views import random_opinion
+
+
+@app.route('/api/opinions/<int:id>/', methods=('GET',))
+def get_opinion(id: int):
+    opinion = Opinion.query.get(id)
+    return jsonify({'opinion': opinion.to_dict()}), 200
+
+
+@app.route('/api/opinions/<int:id>/', methods=('PATCH',))
+def update_opinion(id):
+    data = request.get_json()
+    if (
+            'text' in data and
+            Opinion.query.filter_by(text=data['text']).first() is not None
+    ):
+        return jsonify({'error':
+                            'Такое мнение уже есть в базе данных'}), 400
+    opinion = Opinion.query.get_or_404(id)
+    opinion.title = data.get('title', opinion.title)
+    opinion.text = data.get('text', opinion.text)
+    opinion.source = data.get('source', opinion.source)
+    opinion.added_by = data.get('added_by', opinion.added_by)
+    db.session.commit()
+    return jsonify({'opinion': opinion.to_dict()}), 200
+
+
+@app.route('/api/opinions/<int:id>/', methods=('DELETE',))
+def delete_opinion(id):
+    opinion = Opinion.query.get_or_404(id)
+    db.session.delete(opinion)
+    db.session.commit()
+    return '', 204
+
+
+@app.route('/api/opinions/', methods=['GET'])
+def get_opinions():
+    opinions_list = [opinion.to_dict() for opinion in Opinion.query.all()]
+    return jsonify({'opinions': opinions_list}), 200
+
+
+@app.route('/api/opinions/', methods=('POST',))
+def add_opinion():
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({'error':
+                            'В тело запроса необходимо передать валидный json'}), 400
+    if 'title' not in data or 'text' not in data:
+        return jsonify({'error':
+                            'В запросе отсутствуют обязательные поля'}), 400
+    if Opinion.query.filter_by(text=data['text']).first() is not None:
+        return jsonify({'error':
+                            'Такое мнение уже есть в базе данных'}), 400
+    opinion = Opinion()
+    opinion.from_dict(data)
+    db.session.add(opinion)
+    db.session.commit()
+    return jsonify({'opinion': opinion.to_dict()}), 201
+
+
+@app.route('/api/get-random-opinion/', methods=['GET'])
+def get_random_opinion():
+    opinion = random_opinion()
+    return jsonify({'opinion': opinion.to_dict()}), 200
